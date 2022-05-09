0ae040783d3c36066e0a1a85ec7b7d9e64f3099c
AnGyubin <bkgr4949p@icloud.com>
2022-05-09 23:11:41 +0900

    SetProperty 추가

--- MRD1/ViewModel/BaseViewModel.cs
+++ MRD1/ViewModel/BaseViewModel.cs
@@ -16,9 +16,31 @@ namespace MRD1.ViewModel
         // This method is called by the Set accessor of each property.  
         // The CallerMemberName attribute that is applied to the optional propertyName  
         // parameter causes the property name of the caller to be substituted as an argument.  
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        private void NotifyPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        /// <summary>
+        /// Sets property if it does not equal existing value. Notifies listeners if change occurs.
+        /// </summary>
+        /// <typeparam name="T">Type of property.</typeparam>
+        /// <param name="member">The property's backing field.</param>
+        /// <param name="value">The new value.</param>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
+        protected virtual bool SetProperty<T>(ref T member, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(member, value))
+            {
+                return false;
+            }
+
+            member = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+
     }
 }
